e5244e14602c2ed9eda784339bbe29c7efe1e7fa
Jake Gilbert <jakeg@middlebury.edu>
2024-09-20 13:51:54 -0400

    added key and tooltip updates

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
+//floating tooltip
+const tooltip = document.getElementById("tooltip");
+
+//storing state data json as local var when reload to avoid async functions
+const stateData = [];
+
 /**
  * converts the percentage attribute to a rgb for CSS fill value based on start and end ranges
  */
 function percentageToColor(percentage) {
-    const diffRed = Math.round(249 + (111 - 249) * percentage * 0.01);
-    const diffGreen = Math.round(249 + (174 - 249) * percentage * 0.01);
-    const diffBlue = Math.round(249 + (206 - 249) * percentage * 0.01);
+    const diffRed = Math.round(252 + (0 - 249) * percentage * 0.01);
+    const diffGreen = Math.round(252 + (104 - 249) * percentage * 0.01);
+    const diffBlue = Math.round(252 + (154 - 249) * percentage * 0.01);
     return `rgb(${diffRed}, ${diffGreen}, ${diffBlue})`;
 }
 
 /**
- * fills each state based on percentage
+ * fills each state based on percentage AND stores in state data
  */
 async function stateFill() {
     fetch("stateProgress.json")
@@ -22,13 +28,18 @@ async function stateFill() {
         //same as await response - returned from fetch
         .then((data) => {
             data.forEach((state) => {
-                //const { abbreviation, percentage } = state;
                 const path = document.getElementById(state.abbreviation);
 
                 if (path) {
                     const fillColor = percentageToColor(state.percentage);
                     path.setAttribute("fill", fillColor);
                 }
+                //STORE
+                stateData.push({
+                    state: state.state,
+                    abbreviation: state.abbreviation,
+                    percentage: state.percentage,
+                });
             });
         })
         .catch((error) => {
@@ -36,11 +47,25 @@ async function stateFill() {
         });
 }
 
-const tooltip = document.getElementById("tooltip");
+function getState(abb) {
+    var returnState = {
+        state: "idk",
+        abbreviation: "idk",
+        percentage: 0,
+    };
+    stateData.forEach((state) => {
+        if (state.abbreviation == abb) {
+            returnState = state;
+        }
+    });
+
+    return returnState;
+}
 
 document.querySelectorAll("svg path").forEach((path) => {
     path.addEventListener("mouseenter", (event) => {
-        tooltip.textContent = event.target.getAttribute("id");
+        const state = getState(event.target.getAttribute("id"));
+        tooltip.textContent = `${state.state}: ${state.percentage}% published`;
         tooltip.style.display = "block"; // Show tooltip
     });
 
